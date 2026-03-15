6c460aa5278e597acbaf8b2d0be84dbf029c0000
SPMSPI <kislicin.artur@gmail.com>
2026-03-15 19:10:23 +0200

    ready 2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -12,35 +12,23 @@ import pytest
     ]
 )
 def test_sum_of_the_parts_should_be_equal_to_value(value, number_of_parts, expected) -> None:
-    value = 10
-    number_of_parts = 2
     result = split_integer(value, number_of_parts)
     assert sum(result) == value
 
 
 
+
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(6, 2)
-    assert result == [3, 3]
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 8
-    number_of_parts = 1
-    result = split_integer(value, number_of_parts)
-    assert result == value
+    assert split_integer(8, 1) == [8]
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 17
-    number_of_parts = 5
-    result = split_integer(value, number_of_parts)
-    assert result == sorted(result)
-
+    assert split_integer(17, 5) == sorted(split_integer(17, 5))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 2
-    number_of_parts = 5
-    result = split_integer(value, number_of_parts)
-    assert result == [0, 0, 0, 1, 1]
+    assert split_integer(2, 5) == [0, 0, 0, 1, 1]
 
