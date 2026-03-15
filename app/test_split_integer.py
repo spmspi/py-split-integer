bbe32ba16a9ed7115b3f946fa28ee07282629c91
SPMSPI <kislicin.artur@gmail.com>
2026-03-15 18:53:56 +0200

    ready

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,46 @@
 from app.split_integer import split_integer
+import pytest
 
+@pytest.mark.parametrize(
+    "value, number_of_parts, expected",
+    [
+        (8, 1, [8]),
+        (6, 2, [3, 3]),
+        (17, 4, [4, 4, 4, 5]),
+        (32, 6, [5, 5, 5, 5, 6, 6]),
+        (2, 5, [0, 0, 0, 1, 1])
+    ]
+)
+def test_sum_of_the_parts_should_be_equal_to_value(value, number_of_parts, expected) -> None:
+    value = 10
+    number_of_parts = 2
+    result = split_integer(value, number_of_parts)
+    assert sum(result) == value
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    result = split_integer(6, 2)
+    assert result == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
-
+    value = 8
+    number_of_parts = 1
+    result = split_integer(value, number_of_parts)
+    assert result == value
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    value = 17
+    number_of_parts = 5
+    result = split_integer(value, number_of_parts)
+    assert result == sorted(result)
+
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    value = 2
+    number_of_parts = 5
+    result = split_integer(value, number_of_parts)
+    assert result == [0, 0, 0, 1, 1]
+
