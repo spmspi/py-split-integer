e3363f5047954c77613af689c6cbc6f580e55f2a
SPMSPI <kislicin.artur@gmail.com>
2026-03-15 19:14:36 +0200

    fixed flake8

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,6 +1,7 @@
 from app.split_integer import split_integer
 import pytest
 
+
 @pytest.mark.parametrize(
     "value, number_of_parts, expected",
     [
@@ -11,11 +12,12 @@ import pytest
         (2, 5, [0, 0, 0, 1, 1])
     ]
 )
-def test_sum_of_the_parts_should_be_equal_to_value(value, number_of_parts, expected) -> None:
+def test_sum_of_the_parts_should_be_equal_to_value(
+        value: int,
+        number_of_parts: int,
+        expected: list) -> None:
     result = split_integer(value, number_of_parts)
     assert sum(result) == value
-
-
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -25,10 +27,10 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 1) == [8]
 
+
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert split_integer(17, 5) == sorted(split_integer(17, 5))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(2, 5) == [0, 0, 0, 1, 1]
-
